bd67747e4ff3b1159dcda5590376160808ecc407
Sorawit Trutsat <sorawit@central.tech>
2020-05-16 20:44:19 +0700

    add bloc

---
+++ lib/bloc/get_person_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_movie_app/model/person_response.dart';
+import 'package:flutter_movie_app/repository/repository.dart';
+import 'package:rxdart/subjects.dart';
+
+class PersonsListBloc {
+  final MovieRepository _repository = MovieRepository();
+  final BehaviorSubject<PersonResponse> _subject = BehaviorSubject<PersonResponse>();
+
+  getPersonList() async {
+    PersonResponse response = await _repository.getPersons();
+    _subject.sink.add(response);
+  }
+
+  dispose() {
+    _subject.close();
+  }
+
+  BehaviorSubject<PersonResponse> get subject => _subject;
+}
+
+final personListBloc = PersonsListBloc();
